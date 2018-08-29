cc796bae22ebf27cc95209cfb93bb3314ab6187e
abukhadra <abukhadra@users.noreply.github.com>
2018-08-29 15:01:56 -0400

    fix indentation

--- src/seen/compiler/Category.java
+++ src/seen/compiler/Category.java
@@ -2,21 +2,22 @@ package         seen.compiler;
 
 public enum     Category {
     
-    LINE_TERMINATOR     ( "فاصل_أسطر" )               ,
-    WHITESPACE          ( "مسافة_بيضاء" )               ,    
-    KEYWORD             ( "كلمة_أساسبة" )               ,
-    IDENTIFIER          ( "معرّف" )                 ,
-    MASHREQ_INTEGER     ( "عدد_صحيح_مشرقي" )            ,
-    MAGHREB_INTEGER     ( "عدد_صحيح_مغربي" )            ,
-    MASHREQ_FLOAT       ( "عدد_فاصل_عائم _مشرقي" )          ,
-    MAGHREB_FLOAT       ( "عدد_فاصل_عائم_مغربي" )          ,
-    BOOLEAN             ( "بوليانِيّ" )                 ,
-    CHARACTER           ( "محرف" )                 ,   
-    STRING              ( "سلسلة" )                 ,
-    NULL                ( "خالي" )                  ,
-    SEPARATOR           ( "فاصل" )                 ,
-    OPERATOR            ( "مؤثر" )                  ,
-    ERROR               ( "خطأ" )                  ;          
+            LINE_TERMINATOR     ( "فاصل_أسطر" )         
+    ,       WHITESPACE          ( "مسافة_بيضاء" )             
+    ,       KEYWORD             ( "كلمة_أساسبة" )         
+    ,       IDENTIFIER          ( "معرّف" )            
+    ,       MASHREQ_INTEGER     ( "عدد_صحيح_مشرقي" )      
+    ,       MAGHREB_INTEGER     ( "عدد_صحيح_مغربي" )      
+    ,       MASHREQ_FLOAT       ( "عدد_فاصل_عائم _مشرقي" )   
+    ,       MAGHREB_FLOAT       ( "عدد_فاصل_عائم_مغربي" )    
+    ,       BOOLEAN             ( "بوليانِيّ" )            
+    ,       CHARACTER           ( "محرف" )               
+    ,       STRING              ( "سلسلة" )            
+    ,       NULL                ( "خالي" )            
+    ,       SEPARATOR           ( "فاصل" )           
+    ,       OPERATOR            ( "مؤثر" )            
+    ,       ERROR               ( "خطأ" )                 
+    ;          
     
     private final       String      arabic;
     
